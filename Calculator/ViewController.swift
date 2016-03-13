6fbe48b29e6e5d9839f8208433bb8a2f0cf90de5
Jingjie Zhan <jingjiezhan.jj@gmail.com>
2016-03-12 21:15:12 -0800

    Done with basic operation for calculator.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -23,5 +23,49 @@ class ViewController: UIViewController
             userIsInTheMiddleOfTypingANumber = true
         }
     }
+    
+    var operandStack = Array<Double>()
+    
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        operandStack.append(displayValue)
+        print("Operand Stack = \(operandStack)")
+    }
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue;
+        }
+        set {
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingANumber = false
+        }
+    }
+    
+    func performOperationDoubleOperand(operation: (Double, Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    func performOperationSingleOperand(operation: Double -> Double) {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        switch operation {
+        case "＋": performOperationDoubleOperand{$0 + $1}
+        case "－": performOperationDoubleOperand{$1 - $0}
+        case "✕": performOperationDoubleOperand{$0 * $1}
+        case "÷": performOperationDoubleOperand{$1 / $0}
+        case "√": performOperationSingleOperand{sqrt($0)}
+        default: break
+        }
+    }
 }
 
